c4c4356551cbf2b66275513ccbc1a74513801577
dsorthep <detvisiths@yahoo.com>
2019-11-18 22:37:25 -0600

    html started and questions js created

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        title: "What is the data type with two possible values of true or false?",
+        choices: ["boolean", "jolene", "booski"],
+        answer: "boolean"
+    },
+    {
+        title: "What is the abbreviation for an unordered list tag?",
+        choices: ["uo", "ul", "uno"],
+        answer: "ul"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Castle Style Sheets", "Computer Science Sheets", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "What are the two data types that cannot contain a value?",
+        choices: ["null and undefined", "zero and negative", "none and dead"],
+        answer: "null and undefined"
+    },
+    {
+        title: "What is the Javascript code that is used to perform a task?",
+        choices: ["object", "attribute", "function"],
+        answer: "function"
+    },
+];
