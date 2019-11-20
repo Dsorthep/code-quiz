bf4eb5ee43a0bab1ef9757adfce3ff1a17f72bb0
dsorthep <detvisiths@yahoo.com>
2019-11-19 22:34:42 -0600

    created different functions for start/scoring/questions/check answer/timer

--- assets/questions.js
+++ assets/questions.js
@@ -5,34 +5,34 @@ let questions = [
         choiceA: "boolean", 
         choiceB: "jolene", 
         choiceC: "booski",
-        answer: "A"
+        correct: "A"
     },
     {
         question: "What is the abbreviation for an unordered list tag?",
         choiceA: "uo",
         choiceB: "ul", 
         choiceC: "uno",
-        answer: "B"
+        correct: "B"
     },
     {
         question: "What does CSS stand for?",
         choiceA: "Castle Style Sheets", 
         choiceB: "Computer Science Sheets", 
         choiceC: "Cascading Style Sheets",
-        answer: "C"
+        correct: "C"
     },
     {
         question: "What are the two data types that cannot contain a value?",
         choiceA: "null and undefined", 
         choiceB: "zero and negative", 
         choiceC: "none and dead",
-        answer: "A"
+        correct: "A"
     },
     {
         question: "What is the Javascript code that is used to perform a task?",
         choiceA: "object", 
         choiceB: "attribute", 
         choiceC: "function",
-        answer: "C"
+        correct: "C"
     },
 ];
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,97 @@
 
-//creating the elements
+//creating the constants
 const start = document.getElementById("start-container");
-const quiz = document.getElementById("quiz-container");
-const question = document.getElementById("title");
-const timer = document.getElementById("timer");
-const score = document.getElementById("score");
-const choices = document.getElementById("choices");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const scoreContainer = document.getElementById("score-container");
 
 
 
-
-//function to render the questions 
-// function renderQuestion(){
-
-// }
-
-//function to start the quiz
-start.addEventListener("click", startQuiz());
-
-
-
+start.addEventListener("click", startQuiz()); //to make start disappear on click
+//creating a function to start the quiz
 function startQuiz() {
-start.style.display = "none"; //this is so our start button isn't displayed while the quiz is going
-}; 
+    start.style.display = "none"; //this is so our start button isn't displayed while the quiz is going
+    renderQuestion();
+    quiz.style.display = "block";
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000);
+}
+
+
+//creating variables to run the questions
+let lastQuestion = questions.length -1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 15; //15 seconds per question
+let TIMER
+
+//making the function to present the questions after "Start" is pressed
+function renderQuestion() {
+    let q = questions[runningQuestion]; 
+
+    question.innerHTML = "<p>" + q.question + "</p>"; //this is so the questions from the separate questions.js will show
+    choiceA.innerHTML = q.choiceA; //this will bring the choices from the questions.js
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
 
 
 
-// quiz.addEventListener(renderQuestion());
-// function renderQuestion() {
-    
-//         //this displays the question
-        
-//     quiz.style.display = "block";
-//         let q = questions [runningQuestion];
-//          //creating variables
-//         const lastQuestion = questions.length - 1;
-//         let runningQuestion = 0;
 
-//      question.innerText = "<p>" + q.title + "</p>"; //this displays the question
-//      choiceA.innerText = q.choice[0]; //these are where the choices are in the arrays
-//      choiceB.innerText = q.choice[1];
-//      choiceC.innerText = q.choice[2];
-    
+//making the counter function to time the quiz 
 
-    // }; 
+
+function renderCounter() {
+    if(count<= questionTime) {
+        counter.innerHTML = count;
+        count++
+    }else{
+        count = 0;
+        answerIsWrong();
+        if(runningQuestion < lastQuestion) {
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            clearInterval(TIMER); //ends the quiz and will show  score
+        }
+    }
+}
+
+//making a function to check the answers
+let score = 0
+function checkAnswer(answer) {
+    if( answer === questions[runningQuestion].correct) {
+        score++; //if the answer is correct this adds a point to the score
+        answerIsCorrect();
+    }else{
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion) {
+        count = 0
+        runningQuestion++;
+        renderQuestion();
+    }
+}
+//scoring function
+function scoreRender() {
+    scoreContainer.style.display = "block";
+
+    const scorePercent = Math.round(100 * score/questions.length); //this will calculate score percentage
+}
+
+
+
+
+
+
+
+
+
    
 
  
